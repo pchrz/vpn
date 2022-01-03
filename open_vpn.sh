9c4126fd9bc8d5846934797c5603bca336744f7a
Peter Ch <petchar@ut.utm.edu>
2022-01-03 15:12:21 -0500

    validate input for both ovpn file and password

--- open_vpn.sh
+++ open_vpn.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-	echo "Please provide path to .ovpn file."
-elif [[ "$1" != "*.ovpn" ]]; then
+if [[ -z "$2" ]]; then
+	echo -e "usage: vpn <.ovpn file> <password file>"
+	exit 1
+fi
+
+if [[ $1 != *.ovpn ]]; then
 	echo "Please provide path to a valid .ovpn file"
 else
 	## Supply location and name of .ovpn file. 
 	LOC=$1
+	PASS=$2
 	echo $LOC
-	#sudo openvpn --config /home/pch/VPN/NewYork.ovpn --auth-user-pass /home/pch/pass 
+	echo $PASS
+	#sudo openvpn --config /home/<user>/VPN/NewYork.ovpn --auth-user-pass /home/<user>/pass
+        #sudo openvpn --config $LOC --auth-user-pass $PASS	
 fi
