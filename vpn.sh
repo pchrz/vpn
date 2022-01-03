a8076f5cfd3ec82185c075942d9df0d27b37dfc7
Peter Ch <petchar@ut.utm.edu>
2022-01-03 16:01:33 -0500

    Added comments to vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+##Ensure two arguments have been passed.
 if [[ -z "$2" ]]; then
 	echo -e "usage: vpn <.ovpn file> <username/password file>"
 	exit 1
 fi
 
+##Ensure first argument is a .ovpn file.
 if [[ $1 != *.ovpn ]]; then
 	echo "Please provide path to a valid .ovpn file"
 	exit 1
 fi
+
 LOC=$1
 PASS=$2
 sudo openvpn --config $LOC --auth-user-pass $PASS	
